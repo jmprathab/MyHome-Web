d23b2350fe785f397a73f9e500bd3437314ea185
Prathab Murugan <jm.prathab@gmail.com>
2020-06-22 14:50:17 +0530

    Correcting typo

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -66,7 +66,7 @@ class SignUp extends React.Component {
         const { name, email, password, confirmPassword } = this.state;
         return (
             <div className="sign-up">
-                <h2 className="title">I do not have a account</h2>
+                <h2 className="title">I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className="sign-up-form" onSubmit={this.handleSubmit}>
                     <FormInput
