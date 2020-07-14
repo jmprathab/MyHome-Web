bc6764967b1e79e10e229736c284a487f264adb6
Tobi <43016900+Tobi406@users.noreply.github.com>
2020-07-14 21:15:51 +0200

    Solve errors in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { render } from "@testing-library/react";
+import store from "./redux/store";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Provider store={store}><App /></Provider>);
 });
