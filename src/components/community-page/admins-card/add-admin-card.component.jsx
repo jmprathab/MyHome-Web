cb6104008880790c1ff5e1876a0e2d5059fcbb43
Tobi406 <tobim406@gmail.com>
2020-07-16 15:32:22 +0200

    Add cards

---
+++ src/components/community-page/admins-card/add-admin-card.component.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import Select from "react-select";
+
+import GetUsersApi from "../../../api/GetUsers";
+import AddCommunityAdministratorsApi from "../../../api/AddCommunityAdministrators";
+
+class AddAdminCard extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      data: null,
+      selectedValues: null,
+    }
+  }
+
+  componentDidMount() {
+    const getUsers = async (token) => {
+      let api = new GetUsersApi(token);
+      let responsePromise = api.getUsers();
+      let response = await responsePromise;
+      this.setState({
+        data: response.data.users.map(e => {
+          return {
+            value: e.userId,
+            label: e.name,
+          };
+        }),
+      });
+    }
+    getUsers(this.props.token);
+  }
+
+  handleClick() {
+    const addAdmins = async (selectedValues, uuid, token) => {
+      let api = new AddCommunityAdministratorsApi(selectedValues, uuid, token);
+      let responsePromise = api.addAdministrators();
+      await responsePromise;
+      window.location.reload(false);
+    }
+    addAdmins(this.state.selectedValues, this.props.uuid, this.props.token);
+  }
+
+  handleChange = e => {
+    this.setState({
+      selectedValues: e.map(e => e.value),
+    });
+  }
+
+  render() {
+    return (
+      <div className="w-100 card text-white bg-dark mb-3 d-inline-block">
+        <div className="card-body">
+          <div className="card-text" style={{color: 'black'}}>
+            <Select
+              closeMenuOnSelect={false}
+              isMulti
+              onChange={this.handleChange}
+              options={this.state.data}
+            />
+          </div>
+          <p></p>
+          <button onClick={this.handleClick} className="btn btn-primary">Add administrator</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddAdminCard;
