012182aee17da1e1fc2d90b7c6cf62792d09de45
Tobi406 <tobim406@gmail.com>
2020-07-18 18:55:09 +0200

    Add x to remove admin

--- src/components/community-page/admins-card/admins-card-entry.component.jsx
+++ src/components/community-page/admins-card/admins-card-entry.component.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import GetUserApi from "../../../api/GetUser";
+import RemoveCommunityAdministratorApi from "../../../api/RemoveCommunityAdministrator";
 
 class AdminCardEntry extends Component {
   constructor(props) {
@@ -8,6 +11,17 @@ class AdminCardEntry extends Component {
     this.state = {
       admin: null,
     }
+    this.removeAdmin = this.removeAdmin.bind(this);
+  }
+
+  removeAdmin() {
+    const removeAdmin = async (communityId, uuid, token) => {
+      console.log(communityId, uuid);
+      let api = new RemoveCommunityAdministratorApi(communityId, uuid, token);
+      let responsePromise = api.removeAdministrator();
+      await responsePromise;
+    }
+    removeAdmin(this.props.communityId, this.props.uuid, this.props.token);
   }
 
   componentDidMount() {
@@ -24,7 +38,14 @@ class AdminCardEntry extends Component {
 
   render() {
     return (
-      <>{this.state.admin ? (<li className="list-group-item bg-dark"><a href={`/users/${this.state.admin.userId}`}>{this.state.admin.name}</a></li>) : ''}</>
+      <>{this.state.admin ? (<li className="list-group-item bg-dark">
+          <div className="d-flex justify-content-between">
+            <a href={`/user/${this.state.admin.userId}`}>
+              {this.state.admin.name}
+            </a>
+            <FontAwesomeIcon onClick={this.removeAdmin} icon={faTimes} />
+          </div> 
+        </li>) : ''}</>
     )
   }
 }
