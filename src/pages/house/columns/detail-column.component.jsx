2f69979ac260d683d4ca022f3add90f3bd4582bf
Tobi406 <tobim406@gmail.com>
2020-07-27 11:19:14 +0200

    Remove house delete modal as no method on back-end

--- src/pages/house/columns/detail-column.component.jsx
+++ src/pages/house/columns/detail-column.component.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import HousesApi from "../../../api/Houses";
 import Card from "../../../components/card/card.component";
-import ContentBetween from "../../../components/common/content-between.component";
-import AlignMiddle from "../../../components/common/align-middle.component";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import ModalImpl from "../../../components/modals/modal-impl.component";
 
 class DetailColumn extends Component {
   constructor(props) {
@@ -13,7 +8,6 @@ class DetailColumn extends Component {
 
     this.state = {
       data: null,
-      showModal: false,
     };
   }
 
@@ -27,49 +21,13 @@ class DetailColumn extends Component {
     getData();
   }
 
-  closeModal() {
-    this.setState({
-      showModal: false,
-    });
-  }
-  openModal() {
-    this.setState({
-      showModal: true,
-    });
-  }
-
-  deleteHouse() {
-    // No method on back-end (yet)
-  }
-
   render() {
     return (
       <>
         {this.state.data ? (
-          <>
-            <ModalImpl
-              show={this.state.showModal}
-              closeFunction={this.closeModal}
-              submitFunction={this.deleteHouse}
-              title={`Delete community ${this.state.data.name}`}
-              body={`Are you sure you want to delete this community?`}
-              submitButtonVariant={'danger'}
-              submitText={'Delete'}
-            />
-            <Card
-              header={
-                <ContentBetween>
-                  {this.state.data.name}
-                  <AlignMiddle>
-                    <FontAwesomeIcon
-                      icon={faTimes}
-                      onClick={this.openModal}
-                    />
-                  </AlignMiddle>
-                </ContentBetween>
-              }
-            />
-          </>
+          <Card
+            header={this.state.data.name}
+          />
         ) : ''}
       </>
     )
