fa2c76aa7e2e437a18c5514d224c4278701e0f1c
Tobi406 <tobim406@gmail.com>
2020-09-29 19:47:02 +0200

    Render tests with BrowserRouter

--- src/pages/communities/communities.component.test.js
+++ src/pages/communities/communities.component.test.js
@@ -3,6 +3,7 @@ import {render} from "@testing-library/react";
 import CommunitiesPage from "./communities.component";
 import {Provider} from "react-redux";
 import configureStore from 'redux-mock-store';
+import { BrowserRouter } from "react-router-dom";
 
 describe('Communities Page', () => {
 
@@ -25,23 +26,29 @@ describe('Communities Page', () => {
     const storeWithUserData = mockStore(loggedInUserData);
 
     it('should match snapshot test for home page component', () => {
-        expect(render(<Provider store={storeWithNoUserData}>
-            <CommunitiesPage currentUser={initialData.user.currentUser}/>
-        </Provider>)).toMatchSnapshot()
+        expect(render(<BrowserRouter>
+            <Provider store={storeWithNoUserData}>
+                <CommunitiesPage currentUser={initialData.user.currentUser}/>
+            </Provider>
+        </BrowserRouter>)).toMatchSnapshot()
     });
 
     it('should render welcome message only if user is not having token', () => {
-        const { queryByTestId } = render(<Provider store={storeWithNoUserData}>
-            <CommunitiesPage currentUser={initialData.user.currentUser}/>
-        </Provider>);
+        const { queryByTestId } = render(<BrowserRouter>
+            <Provider store={storeWithNoUserData}>
+                <CommunitiesPage currentUser={initialData.user.currentUser}/>
+            </Provider>
+        </BrowserRouter>);
 
         expect(queryByTestId('welcome-message')).toBeTruthy();
     });
 
     it('should not render welcome message if user is having token', () => {
-        const { queryByTestId } = render(<Provider store={storeWithUserData}>
-            <CommunitiesPage currentUser={loggedInUserData.user.currentUser} />
-        </Provider>)
+        const { queryByTestId } = render(<BrowserRouter>
+            <Provider store={storeWithUserData}>
+                <CommunitiesPage currentUser={loggedInUserData.user.currentUser} />
+            </Provider>
+        </BrowserRouter>)
 
         expect(queryByTestId('welcome-message')).toBeFalsy();
     });
