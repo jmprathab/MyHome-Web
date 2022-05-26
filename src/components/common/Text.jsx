bc6b73e0b9ac7d242ae3175c3858515fcad8ef21
JuanBravo <juanbravo@JuanBravos-MacBook-Pro.local>
2022-05-26 14:30:58 +0300

    Port Text component to FC

--- src/components/common/Text.jsx
+++ src/components/common/Text.jsx
@@ -1,113 +1,133 @@
-import { faChevronDown, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Component } from "react";
-import styled, { css } from "styled-components";
-import styles from "../../styles";
+import {
+  faChevronDown,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, Component } from 'react';
+import styled, { css } from 'styled-components';
+import styles from '../../styles';
 
 const CSS = css`
-  ${props => props.fontSize && css` 
-    font-size: ${props.fontSize};
-  `}
-  ${props => props.textAlign && css`
-    text-align: ${props.textAlign};
-  `}
-  ${props => props.padding && css`
-    padding: ${props.padding};
-  `}
-  ${props => props.margin && css`
-    margin: ${props.margin};
-  `}
-  ${props => (props.bold || props.fontWeight) && css`
-    font-weight: ${props.bold ? 'bold' : (props.fontWeight ? props.fontWeight : '')};
-  `}
-  ${props => props.backgroundColor && css`
-    background-color: ${props.backgroundColor};
-  `}
-  ${props => props.uppercase && css`
-    text-transform: uppercase;
-  `}
-  color: ${props => props.color ? props.color : styles.colors.black};
+  ${(props) =>
+    props.fontSize &&
+    css`
+      font-size: ${props.fontSize};
+    `}
+  ${(props) =>
+    props.textAlign &&
+    css`
+      text-align: ${props.textAlign};
+    `}
+  ${(props) =>
+    props.padding &&
+    css`
+      padding: ${props.padding};
+    `}
+  ${(props) =>
+    props.margin &&
+    css`
+      margin: ${props.margin};
+    `}
+  ${(props) =>
+    (props.bold || props.fontWeight) &&
+    css`
+      font-weight: ${props.bold
+        ? 'bold'
+        : props.fontWeight
+        ? props.fontWeight
+        : ''};
+    `}
+  ${(props) =>
+    props.backgroundColor &&
+    css`
+      background-color: ${props.backgroundColor};
+    `}
+  ${(props) =>
+    props.uppercase &&
+    css`
+      text-transform: uppercase;
+    `}
+  color: ${(props) => (props.color ? props.color : styles.colors.black)};
 
-  ${props => props.dropdown && css`
-    display: inline-block;
+  ${(props) =>
+    props.dropdown &&
+    css`
+      display: inline-block;
 
-    .dropdownContent {
-      display: none;
-      position: absolute;
-    }
+      .dropdownContent {
+        display: none;
+        position: absolute;
+      }
 
-    &[data-dropdownopen = "true"] .dropdownContent {
-      display: block;
-    }
-  `}
+      &[data-dropdownopen='true'] .dropdownContent {
+        display: block;
+      }
+    `}
 
-  ${props => props.label && css`
-    padding: 3px 6px;
-    border-radius: 16px;
-  `}
+  ${(props) =>
+    props.label &&
+    css`
+      padding: 3px 6px;
+      border-radius: 16px;
+    `}
 `;
 
-const Span = styled.span`${CSS}`;
-const Paragraph = styled.p`${CSS}`;
-const Header = styled.h1`${CSS}`;
+const Span = styled.span`
+  ${CSS}
+`;
+const Paragraph = styled.p`
+  ${CSS}
+`;
+const Header = styled.h1`
+  ${CSS}
+`;
 
 const DropdownIcon = styled(FontAwesomeIcon)`
   margin-left: 5px;
   cursor: pointer;
 
   path {
-    transition: d .5s;
+    transition: d 0.5s;
   }
 `;
 const DropdownContent = styled.div`
-  ${props => props.dropdownMargin && css`
-    margin: ${props.dropdownMargin};
-  `}
+  ${(props) =>
+    props.dropdownMargin &&
+    css`
+      margin: ${props.dropdownMargin};
+    `}
 `;
 
-class Text extends Component {
-  constructor(props) {
-    super(props);
+const Text = (props) => {
+  const { type, dropdown, children } = props;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    this.toogleDropdown = this.toogleDropdown.bind(this);
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
-    this.state = {
-      dropdownOpen: false,
-    };
-  }
+  // TODO: Figure out a better way to do this
+  const Selected = (() => {
+    if (type === 'paragraph') return Paragraph;
+    if (type === 'header') return Header;
+    return Span;
+  })();
 
-  toogleDropdown() {
-    const currentState = this.state.dropdownOpen;
-    this.setState({
-      dropdownOpen: !currentState,
-    });
-  }
-
-  render() {
-    let Selected;
-    switch (this.props.type) {
-      case 'paragraph':
-        Selected = Paragraph;
-        break;
-      case 'header':
-        Selected = Header;
-        break;
-      default:
-        Selected = Span;
-        break;
-    }
-    return (
-      <Selected {...this.props} data-dropdownopen={JSON.stringify(this.state.dropdownOpen)}>
-        {this.props.children}
-        {this.props.dropdown && <>
-          <DropdownIcon icon={this.state.dropdownOpen ? faChevronDown : faChevronRight} size="sm" onClick={this.toogleDropdown} />
-          <DropdownContent {...this.props} className="dropdownContent">
-            {this.props.dropdown}
+  return (
+    <Selected {...props} data-dropdownopen={JSON.stringify(dropdownOpen)}>
+      {children}
+      {dropdown && (
+        <>
+          <DropdownIcon
+            icon={dropdownOpen ? faChevronDown : faChevronRight}
+            size="sm"
+            onClick={toggleDropdown}
+          />
+          <DropdownContent {...props} className="dropdownContent">
+            {dropdown}
           </DropdownContent>
-        </>}
-      </Selected>
-    )
-  }
-}
+        </>
+      )}
+    </Selected>
+  );
+};
 
 export default Text;
